f78f03f2742b2de6c69e1075ed03f54e9baa5b93
Carolyn Ryan <ryanc2@carleton.edu>
2017-10-22 19:29:14 -0500

    Updated with translations

--- assignment3.py
+++ assignment3.py
@@ -56,5 +56,61 @@ def parse():
     for tree in parser.parse(sent):
         print(tree)
 
+def clean_sentences(sentence_list):
+    for i in range(len(sentence_list)):
+        sentence = sentence_list[i][:-2]  # remove .\n
+        sentence = sentence.replace('didnt', 'did not')
+        sentence_list[i] = sentence
+    return sentence_list
+
+def get_translations():
+    f = open("translations.txt", "r")
+    english_to_spanish_dict = {}
+    for line in f.readlines():
+        line = line.lower()
+        english_spanish = line.split(":")
+        english = english_spanish[0]
+        spanish = english_spanish[1][:-1] # remove \n
+        english_to_spanish_dict[english] = spanish
+
+    f.close()
+    return english_to_spanish_dict
+
+def translate_sentences():
+    f = open("corpus.txt", "r")
+    english_to_spanish_dict = get_translations()
+    sentence_list = clean_sentences(f.readlines())
+    translated_sentences = []
+    for sentence in sentence_list:
+        word_list = sentence.split()
+        translated_sentence = ""
+        for word in word_list:
+            translated_sentence += english_to_spanish_dict[word.lower()] + " "
+
+        translated_sentences.append(translated_sentence)
+        print("\nEnglish:", sentence)
+        print("Spanish:", translated_sentence)
+    f.close()
+    return translated_sentences
+
+def calc_bleu_score(translated_sentences):
+    f = open("google_translations.txt", "r")
+    google_translated_sentences = clean_sentences(f.readlines())
+    for i in range(len(translated_sentences)):
+        google_sentence = google_translated_sentences[i]
+        our_translation = translated_sentences[i]
+        print(google_sentence, "\n"+our_translation)
+
+    f.close()
+
+
+
 if __name__ == '__main__':
-    main()
+    which_to_run = input("Enter either CFG or T: ").upper()
+    while which_to_run != "CFG" and which_to_run != "T":
+        which_to_run = input("Enter either CFG or T (for translate): ").upper()
+    if which_to_run == "CFG":
+        main()
+    elif which_to_run == "T":
+        translated_sentences = translate_sentences()
+        calc_bleu_score(translated_sentences)
