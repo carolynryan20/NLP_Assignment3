455f51b54f7bc2ac620004226451a6aec9d82cd9
Carolyn Ryan <ryanc2@carleton.edu>
2017-10-23 20:18:38 -0500

    Final?

--- assignment3.py
+++ assignment3.py
@@ -2,20 +2,30 @@ from nltk.tokenize import word_tokenize
 from nltk.grammar import CFG
 from nltk import ChartParser
 from nltk import ngrams
-
-def getCFG():
-    construct_cfg_from_string()
-    parse_original_sentences()
+### Written by Carolyn Ryan
+### Uses defined CFG to parse sentences
+### Uses English word=>Spanish word translation to translate sentences
+### Calculates BLEU score of the translated sentences
 
 def construct_cfg_from_string():
+    '''
+    Reads CFG rules from cfg.txt
+    Uses nltk to make a grammar from the given rules
+    :return: CFG (nltk.grammar.CFG)
+    '''
     f = open("cfg.txt", "r")
     grammar_string = f.readlines()
     grammar = CFG.fromstring(grammar_string)
     f.close()
     return grammar
 
-def parse_original_sentences():
-    grammar = construct_cfg_from_string()
+def parse_original_sentences(grammar):
+    '''
+    Uses given grammar to parse sentences from the file corpus.txt
+    Writes the parse trees of each sentence in parsed_corpus.txt
+    :param grammar: A context free grammar in the form of nltk.grammar.CFG
+    :return: None (Output in parsed_corpus.txt)
+    '''
     parser = ChartParser(grammar)
     f = open("corpus.txt" , "r")
     f_write = open("parsed_corpus.txt", "w")
@@ -38,6 +48,12 @@ def parse_original_sentences():
     print("Parsed form of original corpus sentences using this CFG can be found in parsed_corpus.txt\n")
 
 def clean_sentences(sentence_list):
+    '''
+    Cleans sentences by removing periods and new line characters
+    Gets rid of known contractions in the corpus (in this case replaces didn't with did not)
+    :param sentence_list: A list of sentences
+    :return: Clean list of sentences
+    '''
     for i in range(len(sentence_list)):
         sentence = sentence_list[i][:-2]  # remove .\n
         sentence = sentence.replace('didnt', 'did not') # clean input for improved output
@@ -45,6 +61,11 @@ def clean_sentences(sentence_list):
     return sentence_list
 
 def get_translation_dict():
+    '''
+    Reads direct translations from file translations.txt and makes an English to Spanish dictionary
+    where the key is the word in English and the value is that word translated to Spanish
+    :return: English to Spanish dict
+    '''
     f = open("translations.txt", "r")
     english_to_spanish_dict = {}
     for line in f.readlines():
@@ -57,10 +78,15 @@ def get_translation_dict():
     f.close()
     return english_to_spanish_dict
 
-def translate_sentences():
+def translate_sentences(english_to_spanish_dict):
+    '''
+    Reads sentences from corpus.txt, uses English to Spanish dictionary to translate sentences
+    Writes translated sentences to file translated_sentences.txt
+    :param english_to_spanish_dict: An English to Spanish dictionary {English word: Spanish translation}
+    :return: A list of the sentences in spanish
+    '''
     f = open("corpus.txt", "r")
     f_write = open("translated_sentences.txt", "w")
-    english_to_spanish_dict = get_translation_dict()
     sentence_list = clean_sentences(f.readlines())
     translated_sentences = []
     for sentence in sentence_list:
@@ -77,6 +103,13 @@ def translate_sentences():
     return translated_sentences
 
 def calc_precision(g_tokens, o_tokens, n):
+    '''
+    Calculates precision of an ngram model
+    :param g_tokens: Google's translations (Reference)
+    :param o_tokens: Our translations (Candidate)
+    :param n: desired ngram model
+    :return: Precision of the ngram model candidate with respect to the reference sentence
+    '''
     g_ngrams = list(ngrams(g_tokens, n))
     o_ngrams = list(ngrams(o_tokens, n))
     found_count = 0
@@ -93,6 +126,13 @@ def calc_precision(g_tokens, o_tokens, n):
         return 0
 
 def calc_sentence_bleu(g_tokens, o_tokens, f_write):
+    '''
+    Calculates the BLEU score of a given sentence (given in tokens)
+    :param g_tokens: Google's translation of a sentence (Reference)
+    :param o_tokens: Our translation of the same sentence (Candidate)
+    :param f_write: File to write bleu scores to
+    :return: Sentence BLEU Score
+    '''
     bleu_num = 0
     blue_denom = 0
     sentence = " ".join(o_tokens)
@@ -115,6 +155,12 @@ def calc_sentence_bleu(g_tokens, o_tokens, f_write):
     return bleu
 
 def bleu_score(translated_sentences):
+    '''
+    Calculates BLEU scores for the entire translated corpus using sentences in google_translations.txt as references
+    Writes scores to bleu_scores.txt
+    :param translated_sentences: A list of translated sentences
+    :return: None (output in bleu_scores.txt)
+    '''
     f = open("google_translations.txt", "r")
     f_write = open("bleu_scores.txt", "w")
     google_translated_sentences = clean_sentences(f.readlines())
@@ -133,6 +179,11 @@ def bleu_score(translated_sentences):
     print("BLEU scores for sentences can be seen in bleu_scores.txt")
 
 if __name__ == '__main__':
-    getCFG()
-    translated_sentences = translate_sentences()
+    # Call grammar construction, translate sentences
+    grammar = construct_cfg_from_string()
+    parse_original_sentences(grammar)
+
+    english_to_spanish_dict = get_translation_dict()
+    translated_sentences = translate_sentences(english_to_spanish_dict)
     bleu_score(translated_sentences)
+    
